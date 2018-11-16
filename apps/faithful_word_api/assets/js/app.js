78820a543de647a10ad1669f379c89aa375f518a
Michael Hanna <mykelkim@yahoo.com>
2018-11-16 01:10:23 -0500

    add sass and restore website styles

--- apps/faithful_word_api/assets/js/app.js
+++ apps/faithful_word_api/assets/js/app.js
@@ -1,8 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
-
+// import css from "../css/app.css"
+import css from "../css/app.scss"
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
@@ -11,6 +11,23 @@ import css from "../css/app.css"
 //
 import "phoenix_html"
 
+import "jquery";
+import * as navbar from "./app/navbar.js";
+
+function navbarScroll() {
+  var navbar = document.getElementsByClassName("navbar is-fixed-top")[0];
+  if (navbar && (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)) {
+    navbar.classList.remove("is-transparent");
+  } else {
+    navbar.classList.add("is-transparent");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.onscroll = navbarScroll;
+  navbar.initNavbar();
+  navbarScroll();
+});
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
