bbb2c4fe050bef7a73e869e7864607e3e2f29b7a
Michael Hanna <mykelkim@yahoo.com>
2018-11-18 16:06:22 -0500

    Add faithfulword.app references

--- apps/faithful_word/lib/faithful_word/mailer.ex
+++ apps/faithful_word/lib/faithful_word/mailer.ex
@@ -6,22 +6,22 @@ defmodule FaithfulWord.Mailer do
   def magic_link_email(user, magic_token, _opts) do
     new_email(
       to: user.email,
-      from: "noreply@objectaaron.com",
+      from: "noreply@faithfword.app",
       subject: "Welcome back to FaithfulWord",
       html_body: "<strong>FaithfulWord -- Click the link to login</strong></br></br>
-      Magic Link:</br></br>https://objectaaron.com/login/#{magic_token}",
-      text_body: "Thanks for joining!\n\nClick the magic link:\n\nhttps://objectaaron.com/login/#{magic_token}"
+      Magic Link:</br></br>https://faithfword.app/login/#{magic_token}",
+      text_body: "Thanks for joining!\n\nClick the magic link:\n\nhttps://faithfword.app/login/#{magic_token}"
     )
   end
 
   def welcome_email(user, magic_token, _opts) do
     new_email(
       to: user.email,
-      from: "noreply@objectaaron.com",
+      from: "noreply@faithfword.app",
       subject: "Welcome to FaithfulWord.",
       html_body: "<strong>FaithfulWord -- Click the link to login</strong></br></br>
-      Magic Link:</br></br>https://objectaaron.com/login/#{magic_token}",
-      text_body: "Thanks for joining!\n\nClick the magic link:\n\nhttps://objectaaron.com/login/#{magic_token}"
+      Magic Link:</br></br>https://faithfword.app/login/#{magic_token}",
+      text_body: "Thanks for joining!\n\nClick the magic link:\n\nhttps://faithfword.app/login/#{magic_token}"
     )
   end
 end
