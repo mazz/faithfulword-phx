285622cd6af1c2eff3975e9768c98978695f6124
Michael Hanna <mykelkim@yahoo.com>
2019-01-23 18:20:03 -0500

    Comment-out compile error for now

--- apps/db/lib/db/statistics.ex
+++ apps/db/lib/db/statistics.ex
@@ -1,7 +1,7 @@
 defmodule DB.Statistics do
   import Ecto.Query
 
-  alias DB.Schema.User
+  # alias DB.Schema.User
   alias DB.Repo
 
   @doc """
@@ -16,9 +16,9 @@ defmodule DB.Statistics do
   def all_totals() do
     Repo
     |> Ecto.Adapters.SQL.query!("""
-      SELECT  (select count(id) FROM users) as users, 
+      SELECT  (select count(id) FROM users) as users,
               (select count(id) FROM comments) as comments,
-              (select count(id) FROM statements) as statements, 
+              (select count(id) FROM statements) as statements,
               (select count(id) FROM sources) as sources
     """)
     |> (fn %Postgrex.Result{rows: [[users, comments, statements, sources]]} ->
@@ -29,13 +29,13 @@ defmodule DB.Statistics do
   @doc """
   returns the 20 most active users
   """
-  @spec leaderboard() :: list(%User{})
-  def leaderboard do
-    from(
-      u in User,
-      order_by: [desc: u.reputation],
-      limit: 20
-    )
-    |> Repo.all()
-  end
+  # @spec leaderboard() :: list(%User{})
+  # def leaderboard do
+  #   from(
+  #     u in User,
+  #     order_by: [desc: u.reputation],
+  #     limit: 20
+  #   )
+  #   |> Repo.all()
+  # end
 end
