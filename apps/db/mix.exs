de4aef79bda1ff314b574917f0f72afad2cb7d71
Michael Hanna <mykelkim@yahoo.com>
2019-02-20 22:44:58 -0500

    Preliminary changes for elixir 1.8 support

--- apps/db/mix.exs
+++ apps/db/mix.exs
@@ -9,7 +9,7 @@ defmodule DB.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       compilers: Mix.compilers(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
