2a5a8109ba4c7d43c5b705b53f47b577747f8af2
Antonio Abella <antonioabella@gmail.com>
2019-06-25 09:48:28 -0500

    fix: remove warning message aliases function not used

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule FaithfulWord.Umbrella.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
